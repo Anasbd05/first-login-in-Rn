30ec58fa72cbc25d6a809043d2530c7d3c2eaad0
Anasbd05 <anastrying05@gmail.com>
2025-12-10 10:11:25 +0100

    add search input based on name and type of pokemon

--- app/(tabs)/Pokemon.tsx
+++ app/(tabs)/Pokemon.tsx
@@ -1,6 +1,15 @@
+import Feather from "@expo/vector-icons/Feather";
 import { Link } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { Image, Pressable, ScrollView, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import tw from "twrnc";
 
 interface PokemonT {
@@ -21,39 +30,43 @@ export default function Pokemon() {
   const [pokemons, setPokemons] = useState<PokemonT[]>([]);
   const [shown, setShown] = useState<number>(20);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState<string>("");
 
   useEffect(() => {
-    fetchPokemons();
-  }, [shown]);
+    async function fetchPokemons() {
+      try {
+        setLoading(true);
 
-  async function fetchPokemons() {
-    try {
-      const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/?limit=${shown}`
-      );
-      setLoading(true);
-      const data = await response.json();
+        // Determine the limit based on search
+        const limit = search === "" ? shown : 1000;
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/?limit=${limit}`
+        );
+        const data = await response.json();
 
-      // Fecth details of each pokemon
-      const detailedPokemons = await Promise.all(
-        data.results.map(async (pokemon: any) => {
-          const res = await fetch(pokemon.url);
-          const details = await res.json();
-          return {
-            name: pokemon.name,
-            image: details.sprites.front_default,
-            types: details.types,
-          };
-        })
-      );
+        // Fetch details of each pokemon
+        const detailedPokemons = await Promise.all(
+          data.results.map(async (pokemon: any) => {
+            const res = await fetch(pokemon.url);
+            const details = await res.json();
+            return {
+              name: pokemon.name,
+              image: details.sprites.front_default,
+              types: details.types,
+            };
+          })
+        );
 
-      setPokemons(detailedPokemons);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
+        setPokemons(detailedPokemons);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     }
-  }
+
+    fetchPokemons();
+  }, [shown, search]);
 
   const colorsType: { [key: string]: string } = {
     grass: "#78C850",
@@ -76,12 +89,40 @@ export default function Pokemon() {
     normal: "#A8A878",
   };
 
+  const filteredPokemons = pokemons.filter(
+    (poke) =>
+      poke.name.toLowerCase().includes(search.toLowerCase()) ||
+      poke.types[0].type.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <ScrollView
-      contentContainerStyle={tw` p-4 my-10 pb-24 flex flex-col items-center `}
+      contentContainerStyle={tw` flex flex-col px-5 pb-24 mb-10 bg-white items-center `}
     >
-      <View style={tw`flex flex-row  flex-wrap gap-5s  justify-center`}>
-        {pokemons.map((pokemon, key) => (
+      <View style={tw` flex flex-col gap-2 px-5 mt-16 mb-6 `}>
+        <Text style={tw`text-3xl font-bold text-emerald-900`}>Pokemons</Text>
+        <Text style={tw`font-semibold text-neutral-700 mb-2`}>
+          Search for a Pokemon by name or using its National Pokemon type.
+        </Text>
+        <View
+          style={tw`flex flex-row px-4 gap-3 rounded-xl py-3 items-center bg-gray-100 `}
+        >
+          <Feather name="search" size={22} color="#646464ff" />
+          <TextInput
+            onChangeText={(text) => setSearch(text)}
+            placeholderTextColor="#646464ff"
+            placeholder="Name or type"
+            style={[
+              tw`w-full`,
+              Platform.OS === "web" && { outlineStyle: "none" },
+            ]}
+            value={search}
+          />
+        </View>
+      </View>
+
+      <View style={tw`flex flex-row flex-wrap gap-5 `}>
+        {filteredPokemons.map((pokemon, key) => (
           <Link
             href={{
               pathname: "/PokemonDetails",
@@ -99,24 +140,30 @@ export default function Pokemon() {
                 source={{ uri: pokemon.image }}
                 style={{ width: 150, height: 150 }}
               />
-              <Text style={tw`text-2xl capitalize font-bold text-center`}>
+              <Text
+                numberOfLines={2}
+                style={tw`text-xl capitalize font-bold text-center`}
+              >
                 {pokemon.name}
               </Text>
-              <Text style={tw`text-xl text-neutral-800 text-sm  text-center`}>
+              <Text style={tw`text-xl text-neutral-800 text-sm text-center`}>
                 {pokemon.types[0].type.name}
               </Text>
             </View>
           </Link>
         ))}
       </View>
-      <Pressable
-        onPress={() => setShown(shown + 6)}
-        style={tw`py-2 w-32 flex items-center justify-center mt-8 rounded-lg text-center  bg-emerald-700  `}
-      >
-        <Text style={tw`text-white font-semibold`}>
-          {loading === true ? "Loading..." : "Show more"}
-        </Text>
-      </Pressable>
+
+      {search === "" && (
+        <Pressable
+          onPress={() => setShown(shown + 6)}
+          style={tw`py-2 w-32 flex items-center justify-center mt-8 rounded-lg text-center bg-emerald-700`}
+        >
+          <Text style={tw`text-white font-semibold`}>
+            {loading === true ? "Loading..." : "Show more"}
+          </Text>
+        </Pressable>
+      )}
     </ScrollView>
   );
 }
--- app/PokemonDetails.tsx
+++ app/PokemonDetails.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import tw from "twrnc";
 
 interface DetailsType {
@@ -13,78 +14,98 @@ interface DetailsType {
 }
 
 export default function PokemonDetails() {
-  const [pokeDetails, setPokeDetails] = useState<DetailsType[]>([]);
-
-  useEffect(() => {
-    fetchDetails();
-  });
-
-  const fetchDetails = async () => {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-      const data = await response.json();
-      setPokeDetails([data]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const { name } = useLocalSearchParams();
   const router = useRouter();
+  const [pokeDetails, setPokeDetails] = useState<DetailsType | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${name}`
+        );
+        const data = await response.json();
+        setPokeDetails(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDetails();
+  }, []);
+
+  const stats = [
+    {
+      label: "Base Experience",
+      value: pokeDetails?.base_experience,
+    },
+    {
+      label: "Weight",
+      value: pokeDetails?.weight,
+    },
+    {
+      label: "Height",
+      value: pokeDetails?.height,
+    },
+  ];
+
   return (
     <ScrollView
-      contentContainerStyle={tw` flex bg-[#ECF4E8] rounded-lg  flex-col gap-4 px-4 py-8 h-full `}
+      contentContainerStyle={tw` flex bg-[#ECF4E8] rounded-lg flex-col gap-4 px-4 py-8 h-full `}
     >
-      {pokeDetails.map((detail, key) => (
-        <View key={key}>
-          <View style={tw`flex flex-row items-start gap-20 `}>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View>
+          <View style={tw`flex flex-row mb-6 items-start gap-4 justify-center`}>
             <MaterialIcons
               onPress={() => router.back()}
               name="keyboard-backspace"
-              style={tw`mt-1.5`}
               size={28}
               color="black"
             />
-            <View style={tw`flex flex-col `}>
+            <View style={tw`flex flex-col flex-1 items-center`}>
               <Text
-                style={tw`text-3xl text-emerald-900 text-center font-bold `}
+                style={tw`text-2xl text-emerald-900 text-center font-bold`}
+                numberOfLines={2}
               >
                 {name}
               </Text>
-              <Text style={tw`font-bold text-gray-500 text-center`}>
-                00{detail.id}
+              <Text style={tw`font-bold text-gray-500 text-center mt-1`}>
+                #{pokeDetails?.id}
               </Text>
             </View>
           </View>
 
-          <View style={tw` flex flex-col gap-2 mt-12`}>
-            <View style={tw` flex flex-row gap-2`}>
-              <Text style={tw` font-semibold text-xl text-neutral-800 `}>
-                Base Experience:
-              </Text>
-              <Text style={tw` font-bold text-xl text-emerald-900 `}>
-                {detail.base_experience}
-              </Text>
-            </View>
-            <View style={tw` flex flex-row gap-2`}>
-              <Text style={tw` font-semibold text-xl text-neutral-800 `}>
-                Weight:
-              </Text>
-              <Text style={tw` font-bold text-xl text-emerald-900 `}>
-                {detail.weight}
-              </Text>
-            </View>
-            <View style={tw` flex flex-row gap-2`}>
-              <Text style={tw` font-semibold text-xl text-neutral-800 `}>
-                Height:
-              </Text>
-              <Text style={tw` font-bold text-xl text-emerald-900 `}>
-                {detail.height}
-              </Text>
-            </View>
+          <View
+            style={tw` bg-[#5A9CB550] h-[200px] rounded-xl justify-center flex items-center`}
+          >
+            <Image
+              source={{ uri: pokeDetails?.sprites.front_default }}
+              style={tw`h-full w-[230px]`}
+            />
+          </View>
+
+          <View style={tw` flex flex-col gap-4 mt-12`}>
+            {stats.map((stat, key) => (
+              <View
+                key={key}
+                style={tw` flex flex-row gap-2 justify-between shadow-sm rounded-lg p-3 bg-emerald-100`}
+              >
+                <Text style={tw` font-semibold text-xl text-neutral-800`}>
+                  {stat.label} :
+                </Text>
+                <Text style={tw` font-bold text-xl text-emerald-900`}>
+                  {stat.value}
+                </Text>
+              </View>
+            ))}
           </View>
         </View>
-      ))}
+      )}
     </ScrollView>
   );
 }
